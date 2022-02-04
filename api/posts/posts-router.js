34e1023d4a9d79b65c4050f8fc5320415c20fbe0
Evan <evan.dieterich.az@gmail.com>
2022-02-04 16:40:16 -0700

    server running, get / working, posts route established

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+    .then(posts => {
+        res.json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved'
+        })
+    })
+})
+
+
+module.exports = router;
